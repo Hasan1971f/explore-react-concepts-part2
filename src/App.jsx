9eaa80e15a3d05490c4403e35457b95ab95b1b6a
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-18 08:35:49 +0600

    added a understand the concept of  use Effect

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import Counter from './Counter'
 import Team from './Team'
+import Users from './Users'
 
 function App() {
   function handleClick(){
@@ -19,6 +20,7 @@ function App() {
     <>
       
       <h3>React Core Concepts 2</h3>
+      <Users></Users>
       <Team></Team>
       
       <Counter></Counter>
