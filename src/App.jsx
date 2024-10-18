b5604e87f536037891f83f20388e240567ba5137
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-18 07:44:37 +0600

    add a use state and React hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Counter from './Counter'
-
+import Team from './Team'
 
 function App() {
   function handleClick(){
@@ -19,6 +19,8 @@ function App() {
     <>
       
       <h3>React Core Concepts 2</h3>
+      <Team></Team>
+      
       <Counter></Counter>
       
       
